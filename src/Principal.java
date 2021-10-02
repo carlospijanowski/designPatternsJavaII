a1a51a7c8220a200a0f78cbbefb09d21b8a922fc
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-10-02 18:08:20 -0300

    removendo metodos desnecessarios

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,7 @@
 import java.math.BigDecimal;
 
 public class Principal {
-
-    private static BigDecimal caulculoDeMortes(){
-        return (new BigDecimal(1*2.3));
-    }
-
-
-
     public static void main(String[] args) {
-        System.out.println(caulculoDeMortes().toString());
+        System.out.println("testes");
     }
-
 }
