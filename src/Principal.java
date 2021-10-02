29b9bea9ebda01c3f99e5afe718401ff17332fb9
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-10-01 21:35:41 -0300

    terceiro commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,13 @@
+import java.math.BigDecimal;
+
 public class Principal {
-    public static void main(String[] args) {
-    System.out.println("teste");
+
+    private static BigDecimal caulculoDeMortes(){
+        return (new BigDecimal(1*2.3));
     }
+
+    public static void main(String[] args) {
+        System.out.println(caulculoDeMortes().toString());
+    }
+
 }
